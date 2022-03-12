f0ca5dd36e54e88ff24d06f94e1778650b27802c
Chialiang86 <a930039930039@gmail.com>
2022-03-12 23:57:45 +0800

    err 0.21

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 using namespace std;
 
+#define MINIMUM 1e-10
+
 typedef struct _datapair {
     unsigned char row;
     unsigned char col;
@@ -90,36 +92,102 @@ void display(datapair dp) {
         }
         cout << endl;
     }
+    cout << endl;
 }
 
-void dump_histogram(vector<vector<unsigned>> histograms) {
-    for (unsigned i = 0; i < histograms.size(); i++) {
-        for (unsigned b = 0; b < 32; b++) {
-            cout << setw(5) << histograms[i][b];
+vector<unsigned> count_category_num(vector<datapair> &dp_vec) {
+    vector<unsigned> ret(10, 0);
+    for (int i = 0; i < dp_vec.size(); i++)
+        ret[dp_vec[i].label] ++;
+    return ret;
+}
+
+vector<unsigned char> count_bins(datapair &dp) {
+    unsigned total = dp.row * dp.col;
+    vector<unsigned char> bins(total, 0);
+    for (unsigned i = 0; i < total; i++) {
+        bins[i] = dp.img[i] / 8;
+    }
+    return bins;
+}
+
+vector<vector<vector<double>>> create_table(vector<datapair>& dp_vec, double init_num=1e-10) {
+    vector<vector<vector<double>>> ret(10, vector<vector<double>>(784, vector<double>(32, init_num)));
+
+    for (unsigned i = 0; i < dp_vec.size(); i++) {
+        for (unsigned b = 0; b < 784; b++) {
+            ret[dp_vec[i].label][b][dp_vec[i].img[b] / 8] += 1;
+        }
+    }
+
+    return ret;
+}
+
+void dump_table(vector<vector<vector<double>>>& train_table){
+    for (int i = 0; i < train_table.size(); i++) {
+        for (int j = 0; j < train_table[i].size(); j++) {
+            for (int k = 0; k < train_table[i][j].size(); k++) {
+                cout << train_table[i][j][k] << " ";
+            }
+            cout << endl;
         }
         cout << endl;
     }
 }
 
-vector<unsigned> count_histogram(datapair &dp) {
-    vector<unsigned> histogram(32, 1);
-    for (unsigned char i = 0; i < dp.row; i++) {
-        for (unsigned char j = 0; j < dp.row; j++) {
-            histogram[((unsigned)dp.img[i * dp.row + j] / 8)]++;
+vector<double> count_discrete_posterior(vector<vector<vector<double>>>& train_table, vector<unsigned> category_nums, datapair test_img) {
+    vector<double> prediction(10, 0.0);
+    vector<unsigned char> test_bin = count_bins(test_img);
+
+    double total = 0;
+    double p_bi, p_bni, num_bi, num_bni;
+
+    for (int i = 0; i < category_nums.size(); i++) {
+        total += category_nums[i];
+    }
+    
+    for (unsigned i = 0; i < train_table.size(); i++) {
+        p_bi = p_bni = 1.0;
+        for (unsigned bin = 0; bin < test_bin.size(); bin++) {
+            num_bi = num_bni = 0.0;
+            for (unsigned category = 0; category < train_table.size(); category++) {
+                if (i == category) {
+                    num_bi = train_table[category][bin][test_bin[bin]];
+                } else {
+                    num_bni += train_table[category][bin][test_bin[bin]];
+                }
+            }
+            p_bi *= (num_bi / category_nums[i]);
+            p_bni *= (num_bni / (total - category_nums[i]));
+        }
+
+        p_bi *= (category_nums[i] / total);
+        p_bni *= ((total - category_nums[i]) / total);
+        prediction[i] = p_bi / (p_bi + p_bni);
+        if (prediction[i] < 0.0) {
+            cout << "negative" << endl;
+            exit(0);
         }
     }
-    return histogram;
+
+    return prediction;
 }
 
-vector<vector<vector<unsigned>>> create_prior(vector<datapair> &dp_vec) {
-    vector<vector<vector<unsigned>>> ret(10, vector<vector<unsigned>>());
+int judge_pred(vector<double> pred, unsigned char ans) {
+    cout << "Posterior (probabilities)" << endl;
 
-    for (unsigned i = 0; i < dp_vec.size(); i++) {
-        ret[dp_vec[i].label].push_back(count_histogram(dp_vec[i]));
+    double max = -1;
+    unsigned char pre = -1;
+    for (unsigned char i = 0; i < pred.size(); i++) {
+        cout << (unsigned)i << ": " << pred[i] << endl;
+        if (pred[i] > max) {
+            max = pred[i];
+            pre = i;
+        }
     }
-
-    return ret;
-} 
+    cout << "Prediction: " << (unsigned)pre << ", Ans: " << (unsigned)ans << endl << endl;
+    return pre == ans ? 0 : 1;
+}
 
 int main(int argc, char * argv[]) {
     const string fname_train_img = "train-images.idx3-ubyte";
@@ -127,17 +195,35 @@ int main(int argc, char * argv[]) {
     const string fname_test_img = "t10k-images.idx3-ubyte";
     const string fname_test_label = "t10k-labels.idx1-ubyte";
 
+    int mode;
+    double init_val;
+    cout << "input option (0 for discrete mode, 1 for continuous mode) : ";
+    cin >> mode;
+    if (mode == 0) {
+        cout << "input init value : ";
+        cin >> init_val;
+        init_val = init_val < 0 ? 1e-10 : init_val;
+    }
+
     vector<datapair> train_data =  read_datapairs(fname_train_img, fname_train_label);
     vector<datapair> test_data =  read_datapairs(fname_test_img, fname_test_label);
 
-    vector<vector<vector<unsigned>>> train_hist_0to9 = create_prior(train_data);
-    vector<vector<vector<unsigned>>> test_hist_0to9 = create_prior(test_data);
+    vector<unsigned> category_nums = count_category_num(train_data);
+    vector<vector<vector<double>>> train_table = create_table(train_data, init_val);
 
-    dump_histogram(train_hist_0to9[0]);
+    vector<double> pred;
+    double error_rate = 0.0;
+    for (int i = 0; i < test_data.size(); i++) {
+        pred = count_discrete_posterior(train_table, category_nums, test_data[i]);
+        error_rate += judge_pred(pred, test_data[i].label);
+    }
+    error_rate /= test_data.size();
 
-    display(train_data[0]);
-    display(test_data[0]);
+    cout << "Imagination of numbers in Bayesian classifier: " << endl;
+    for (int i = 0; i < test_data.size(); i++) 
+        display(test_data[i]);
 
+    cout << "Error rate: " << error_rate << endl;
 
     return 0;
 
