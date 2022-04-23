3789df2f36a1c927a11c3e20d079a42b108348a1
Chialiang86 <a930039930039@gmail.com>
2022-04-23 19:31:02 +0800

    hw4 : 85%

--- HW4/EM.cpp
+++ HW4/EM.cpp
@@ -1,9 +1,11 @@
+#include <iomanip>
 #include <vector>
 #include <cmath>
 #include <iomanip>
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <unistd.h>
 using namespace std;
 
 #define MINIMUM 1e-5
@@ -40,7 +42,7 @@ vector<datapair> read_datapairs(const string& img_file, const string& label_file
 
     // magic number
     fin_img.read(buffer, 4);
-    unsigned magic = byte2unsigned(buffer);
+    byte2unsigned(buffer);
 
     // length
     fin_img.read(buffer, 4);
@@ -55,8 +57,6 @@ vector<datapair> read_datapairs(const string& img_file, const string& label_file
     // dummy of label
     fin_label.read(dummy, 8);
 
-    cout << "magic num = " << magic << " num of data = " << num_data << " row = " << row << " col = " << col << endl;
-    
     char byte;
     unsigned num_pixels = row * col;
     for (unsigned i = 0; i < num_data; i++) {
@@ -102,89 +102,105 @@ void display(datapair& dp, unsigned char pred) {
     cout << endl;
 }
 
-vector<unsigned> count_category_num(vector<datapair> &dp_vec) {
-    vector<unsigned> ret(10, 0);
-    for (long unsigned i = 0; i < dp_vec.size(); i++)
-        ret[dp_vec[i].label] ++;
-    return ret;
-}
-
-// vector<unsigned> count_k(vector<datapair> &dp_vec) {
-//     unsigned total = dp_vec[0].row * dp_vec[0].col;
-//     vector<unsigned> k(dp_vec.size(), 0);
-
-//     for (long unsigned i = 0; i < dp_vec.size(); i++) {
-//         for (unsigned b = 0; b < total; b++) {
-//             k[i] += dp_vec[i].img[b] / 128;
-//         }
-//     }
-//     return k;
-// }
-
 vector<double> init_lambda(vector<unsigned> &category_num, unsigned total) {
     vector<double> ret(category_num.size(), 0);
     for (unsigned long i = 0; i < category_num.size(); i++) {
         ret[i] = (double)(category_num[i]) / total;
     }
-
     return ret;
 }
 
 vector<vector<double> > init_p() {
-    vector<vector<double> > p(10, vector<double>(784, 0.5));
+    vector<vector<double> > p(10, vector<double>(784, 0.0));
+    for (unsigned char j = 0; j < 10; j++) {
+        for (unsigned bin = 0; bin < 784; bin++) {
+            p[j][bin] = 0.4 + ((double) rand() / (RAND_MAX)) / 5; // range from [0.4 -> 0.6]
+        }
+    }
     return p;
 }
 
 void E_step(const vector<datapair> &train_data, const vector<vector<double> > &p, const vector<double> &lambda, vector<vector<double> > &w) {
-    double sum;
+    double w_dividend;
     for (unsigned long i = 0; i < train_data.size(); i++) {
-        sum = 0.0;
+        w_dividend = 0.0;
         for (unsigned char j = 0; j < 10; j++) {
             w[j][i] = lambda[j]; 
             for (unsigned bin = 0; bin < 784; bin++) {
                 w[j][i] *= train_data[i].img[bin] ? p[j][bin] : (1 - p[j][bin]);
             }
-            sum += w[j][i];
+            w_dividend += w[j][i];
         }
         for (unsigned char j = 0; j < 10; j++) {
-            w[j][i] /= sum;
-            // cout << "w[" << (unsigned)j << "][" << i << "] = " << w[j][i] << endl;
+            w[j][i] /= w_dividend;
         }
     }
 }
 
 void M_step(const vector<datapair> &train_data, const vector<vector<double> > &w, vector<vector<double> > &p, vector<double> &lambda) {
-    double sum;
-    vector<double> w_sum(10, 0.0);
+    double lambda_dividend, p_dividend;
     // count lambda
     for (unsigned char j = 0; j < 10; j++) {
-        sum = 0.0;
+        lambda_dividend = 0.0;
         for (unsigned long i = 0; i < train_data.size(); i++) {
-            sum += w[j][i];
+            lambda_dividend += w[j][i];
         }
-        w_sum[j] = sum;
-        lambda[j] = sum / train_data.size();
-        // cout << "l[" << (unsigned)j << "] = " << lambda[j] << " ";
-    }
-    cout << endl;
+        lambda[j] = lambda_dividend / train_data.size();
 
-    // count p
-    for (unsigned char j = 0; j < 10; j++) {
         for (unsigned bin = 0; bin < 784; bin++) {
-            sum = 0.0;
+            p_dividend = 0.0;
             for (unsigned long i = 0; i < train_data.size(); i++) {
-                sum += (w[j][i] * train_data[i].img[bin]);
+                p_dividend += (w[j][i] * train_data[i].img[bin]);
             }
-            p[j][bin] = sum / w_sum[j];
+            p[j][bin] = p_dividend / lambda_dividend;
         }
     }
 }
 
-void inference(vector<vector<double> > &p) {
+void count_zw_map(const vector<datapair> &train_data, const vector<vector<double> > &w, vector<unsigned char> &zw_map) {
+    vector<vector<unsigned>> zw_cnt(10, vector<unsigned>(10, 0));
+    double max;
+    unsigned long max_j;
+    for (unsigned long i = 0; i < train_data.size(); i++) {
+        max = -1;
+        max_j = -1;
+        for (unsigned long j = 0; j < 10; j++) {
+            if (w[j][i] > max) {
+                max = w[j][i];
+                max_j = j;
+            }
+        }
+        zw_cnt[train_data[i].label][max_j]++;
+    }
+
+    for (unsigned long i = 0; i < 10; i++) {
+        max = -1;
+        max_j = -1;
+        for (unsigned long j = 0; j < 10; j++) {
+            if (zw_cnt[i][j] > max) {
+                max = zw_cnt[i][j];
+                max_j = j;
+            }
+        }
+        zw_map[i] = max_j;
+    }
+}
+
+double inference(const vector<datapair> &train_data, const vector<unsigned char> &zw_map, const vector<vector<double> > &p, const vector<vector<double> > &p_cache) {
+    unsigned char cur_w;
+    double diff = 0.0;
+
+    for (unsigned long i = 0; i < 10; i++) {
+        for (unsigned bin = 0; bin < 784; bin++) {
+            diff += abs(p[i][bin] - p_cache[i][bin]);
+        }
+    }
+
     for (unsigned char j = 0; j < 10; j++) {
         cout << "class " << (unsigned)j << ":" << endl;
+        cur_w = j;
         for (unsigned bin = 0; bin < 784; bin++) {
-            cout << (p[j][bin] > 0.3 ? 1 : 0) << " ";
+            cout << (p[cur_w][bin] > 0.5 ? 1 : 0) << " ";
             if (bin % 28 == 27)
                 cout << endl;
         }
@@ -192,98 +208,74 @@ void inference(vector<vector<double> > &p) {
     }
     cout << endl;
     fflush(stdout);
+
+    return diff;
 }
 
-// double count_p(double lambda_j, double p_j, unsigned k_i, unsigned pixels=784) {
-//     double exp_10 = 0, ret;
-//     // log(C(pixels, k_i)) * p_j^k_i * (1 - p_j)^(pixels - k_i))
+void confusion_matrix(const vector<datapair> &train_data, const vector<unsigned char> &zw_map, const vector<vector<double> > &w) {
+    vector<unsigned char> pred(train_data.size(), 0);
+    unsigned tp, fn, fp, tn;
+    double max;
+    unsigned char max_j;
+    for (unsigned long i = 0; i < train_data.size(); i++) {
+        max = 0;
+        for (unsigned long j = 0; j < 10; j++) {
+            if (w[zw_map[j]][i] > max) {
+                max = w[zw_map[j]][i];
+                max_j = j;
+            }
+        }
+        pred[i] = max_j;
+    }
 
-//     // log (C(pixels, k_i)) = log (pixels! / (pixels - k_i)!) - log (k_i!)
-//     for (unsigned i = pixels - k_i + 1; i < pixels; i++) {
-//         exp_10 += log10(i); // log (pixels! / (pixels - k_i)!)
-//     }
-//     for (unsigned i = 1; i < k_i; i++) {
-//         exp_10 -= log10(i); // log (k_i!)
-//     }
-
-//     // log(p_j^k_i * (1 - p_j)^(pixels - k_i))
-//     exp_10 += (k_i * log10(p_j));
-//     exp_10 += ((pixels - k_i) * log10(1.0 - p_j));
-//     exp_10 += log10(lambda_j);
-//     ret = pow(10.0, exp_10);
-//     return exp_10;
-// }
+    for (unsigned long j = 0; j < 10; j++) {
+        tp = fn = fp = tn = 0;
+        for (unsigned long i = 0; i < train_data.size(); i++) {
+            if (train_data[i].label == j) {
+                if (pred[i] == j) {
+                    tp++;
+                } else {
+                    fn++;
+                }
+            } else {
+                if (pred[i] == j) {
+                    fp++;
+                } else {
+                    tn++;
+                }
+            }
+        }
+        cout << "Confusion Matrix " << j << ":" << endl;
+        cout << "Is number " << j << setw(13) << tp << setw(10) << fn << endl;
+        cout << "Isn't number " << j << setw(10) << fp << setw(10) << tn << endl << endl;
+        cout << "Sensitivity (Successfully predict number " << j << ")      : " << (double)tp / (tp + fn) << endl; 
+        cout << "Sensitivity (Successfully predict not number " << j << ")  : " << (double)tn / (fp + tn) << endl; 
+    }
+}
 
 vector<vector<double> > train_p_lambda(const vector<datapair> &train_data, vector<double> &lambda, vector<vector<double> > &p) {
     vector<vector<double>> w(10, vector<double>(train_data.size(), 1.0));
+    vector<vector<double> > p_cache;
+    vector<unsigned char> zw_map(10, -1);
 
-    int cnt = 0;
-    double w_sum, divident, divisor;
-    while (cnt < 100) {
-        // for (int j = 0; j < 10; j++) {
-        //     cout << "lambda " << j << " = " << lambda[j] << endl;
-        // }
-        // for (int j = 0; j < 10; j++) {
-        //     cout << "p " << j << " = " << p[j] << endl;
-        // }
-        // cout << endl;
-        
+    int cnt = 0, max_cnt = 100;
+    double diff, eps = 10;
+    while (cnt < max_cnt) {
         cnt ++;
-        cout << "[iteration = " << cnt << "] E step running..." << endl;
+        p_cache = p;
         E_step(train_data, p, lambda, w);
-        cout << "[iteration = " << cnt << "] M step running..." << endl;
         M_step(train_data, w, p, lambda);
-        inference(p);
-
-        // // E step
-        // for (int ki = 0; ki < 785; ki++) {
-        //     divisor = 0.0;
-        //     for (int i = 0; i  < 10; i++) {
-        //         w[i][ki] = count_p(lambda[i], p[i], ki);
-        //         divisor += w[i][ki];
-        //     }
-        //     for (int i = 0; i < 10; i++) {
-        //         w[i][ki] /= divisor;
-        //         printf("w[%d][%d] = %f\n", i, ki, w[i][ki]);
-        //     }
-        // }
-        
-        // // M step
-        // for (int i = 0; i < 10; i++) {
-        //     w_sum = divident = divisor = 0;
-        //     for (int id = 0; id < n; id++) {
-        //         w_sum += w[i][k[id]];
-        //         divident += w[i][k[id]] * k[id]; // sum(w_id_i * k_id) from id = 0 -> n
-        //         divisor += w[i][k[id]] * 784;
-        //     }
-        //     lambda[i] = w_sum / n;
-        //     p[i] = divident / divisor;
-        //     // cout << "p[" << i << "] = " << p[i] << " ";
-        // }
-        // cout << endl;
-
+        count_zw_map(train_data, w, zw_map);
+        diff = inference(train_data, zw_map, p, p_cache);
+        cout << "No. of Iteration: " << cnt << ", Difference: " << diff << endl << endl;
+        if (diff < eps) {
+            break;
+        }
     }
+    confusion_matrix(train_data, zw_map, w);
 
     return w;
 }
-
-// vector<unsigned char> inference(vector<vector<double>> &w, vector<unsigned> &k, vector<datapair> &test_data) {
-//     unsigned long test_num = k.size();
-//     vector<unsigned char> ret(test_num, -1);
-//     double max;
-    
-//     for (unsigned long id = 0; id < test_num; id++) {
-//         max = -1;
-//         for (unsigned long i = 0; i < 10; i++) {
-//             if (max < w[i][k[id]]) {
-//                 max = w[i][k[id]];
-//                 ret[id] = i;
-//             }
-//         }
-//         cout << "predict : " << ret[id] << " answer : " << test_data[id].label << endl;
-//     }
-//     return ret;
-// }
 
 int main(int argc, char * argv[]) {
     const string fname_train_img = "train-images.idx3-ubyte";
@@ -292,17 +284,11 @@ int main(int argc, char * argv[]) {
     const string fname_test_label = "t10k-labels.idx1-ubyte";
 
     vector<datapair> train_data =  read_datapairs(fname_train_img, fname_train_label);
-    // vector<datapair> test_data =  read_datapairs(fname_test_img, fname_test_label);
 
-    vector<unsigned> category_nums = count_category_num(train_data);
-    
-    // vector<unsigned> k_train = count_k(train_data);
-    // vector<unsigned> k_test = count_k(train_data);
-    vector<double> lambda = init_lambda(category_nums, train_data.size()); // for training
+    srand(getpid() ^ time(0));
+    vector<double> lambda(10, 0.1);
     vector<vector<double> > p = init_p(); // for training
-
     vector<vector<double> > w = train_p_lambda(train_data, lambda, p);
-    // vector<unsigned char> res = inference(w, k_test, test_data);
     return 0;
 
 }
